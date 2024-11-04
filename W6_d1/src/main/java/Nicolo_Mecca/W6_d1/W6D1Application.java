392b9f55659261820b8872f902e47f523ec7e836
Nicolomecca <nico97.mecca@gmail.com>
2024-11-04 11:27:08 +0100

    configurazione

---
+++ W6_d1/src/main/java/Nicolo_Mecca/W6_d1/W6D1Application.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.W6_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W6D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W6D1Application.class, args);
+	}
+
+}
---
+++ W6_d1/src/test/java/Nicolo_Mecca/W6_d1/W6D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.W6_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
